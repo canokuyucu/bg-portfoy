f148c1328333b1c2628ef796765a2d2a8610906c
canokuyucu <103751091+canokuyucu@users.noreply.github.com>
2026-07-20 11:27:55 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="ayaz-portfoy-v1";
+const FILES=["./","index.html","manifest.webmanifest","icon-192.png","icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));});
+self.addEventListener("activate",e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
